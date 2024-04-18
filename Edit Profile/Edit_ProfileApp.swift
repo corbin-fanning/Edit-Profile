f09ac6b0512b5885309fab8beb75feac64f535d2
Corbin Fanning <corbin.hfanning@tamu.edu>
2024-04-18 13:14:44 -0500

    Initial Commit

---
+++ Edit Profile/Edit_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edit_ProfileApp.swift
+//  Edit Profile
+//
+//  Created by Fanning, Corbin on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Edit_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
